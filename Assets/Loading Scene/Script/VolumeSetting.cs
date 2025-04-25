7300cd366fb8b576f87591fa6255535bb70db598
Momosaki427 <muhsyifa234@gmail.com>
2025-04-25 22:58:57 +0700

    Salah
    tlg revisi

---
+++ Assets/Loading Scene/Script/VolumeSetting.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSetting : MonoBehaviour
+{
+    [SerializeField] private AudioMixer Mixer;
+    [SerializeField] private Slider SC;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("Music"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetMusicVolume();
+        }
+    }
+
+    public void SetMusicVolume()
+    {
+        float volume = SC.value;
+        Mixer.SetFloat("Music", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+
+    private void LoadVolume()
+    {
+        SC.value = PlayerPrefs.GetFloat("musicVolume");
+
+        SetMusicVolume();
+    }
+}
